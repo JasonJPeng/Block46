e879dc3569b1cc7408df4aac0aaa42a166e6baa8
Jason Peng <jason.j.peng@gamil.com>
2019-08-23 11:07:33 -0700

    date works

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -22,7 +22,7 @@ class LineChart extends Component {
 			let data = []
             for (let i=1; i<history.data.Time.length; i++) {
 			   data.push({
-							 x: parseInt(history.data.Time[i]/86400) ,
+							 x: new Date(history.data.Time[i]*1000) ,
 							 y: history.data.Price[i]
 			             })
 			} 
@@ -30,33 +30,6 @@ class LineChart extends Component {
 			self.setState({data: data})
 	})
 	}
-
-
-//    data1 = [
-// 	{ x: 1, y: 64 },
-// 	{ x: 2, y: 61 },
-// 	{ x: 3, y: 64 },
-// 	{ x: 4, y: 62 },
-// 	{ x: 5, y: 64 },
-// 	{ x: 6, y: 60 },
-// 	{ x: 7, y: 58 },
-// 	{ x: 8, y: 59 },
-// 	{ x: 9, y: 53 },
-// 	{ x: 10, y: 54 },
-// 	{ x: 11, y: 61 },
-// 	{ x: 12, y: 60 },
-// 	{ x: 13, y: 55 },
-// 	{ x: 14, y: 60 },
-// 	{ x: 15, y: 56 },
-// 	{ x: 16, y: 60 },
-// 	{ x: 17, y: 59.5 },
-// 	{ x: 18, y: 63 },
-// 	{ x: 19, y: 58 },
-// 	{ x: 20, y: 54 },
-// 	{ x: 21, y: 59 },
-// 	{ x: 22, y: 64 },
-// 	{ x: 23, y: 59 }
-// ] 
 
 
 	render() {
@@ -73,13 +46,14 @@ class LineChart extends Component {
 				prefix: "$"
 			},
 			axisX: {
-				title: "Days",
-				prefix: "D",
+				title: "Date",
+				xValueType: "dateTime",
+				prefix: " "
 				// interval: 2
 			},
 			data: [{
 				type: "line",
-				toolTipContent: "Week {x}: {y}%",
+				toolTipContent: " {x}: $ {y}",
 				dataPoints: this.state.data
 			}]
 		}
