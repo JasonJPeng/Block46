d28f835592e7175f4a85197b64e73470c6eb41d9
Jason Peng <jason.j.peng@gamil.com>
2019-08-23 16:52:25 -0700

    normalize

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -6,13 +6,30 @@ import axios from "axios";
 class LineChart extends Component {
 	
 	state = {
-		data : []
+		data : [],
+		title: ""
 	}
 	
 	componentDidMount() {
          this.getData(this.props.Ids);
 	}
 	
+	normalizeChart = (event) => {
+		event.preventDefault();
+		let newData = [];
+		console.log("State Data=> ", this.state.data)
+		for (let i=0; i < this.state.data.length; i++) {
+			let maxValue = Math.max(...this.state.data[i].dataPoints.map(element=>element.y));
+			let factor = 10 ** parseInt(Math.log10(maxValue));
+			console.log("factor->", factor)	
+			let newPoints = this.state.data[i];
+			newPoints.dataPoints.map(ele=> ele.y = ele.y/factor)
+			newData.push(newPoints)				
+		}
+
+		this.setState({data: newData})
+		
+	}
 
 	getDataPoints = (id) => {
 		return new Promise((resolve, reject) => {
@@ -48,6 +65,8 @@ class LineChart extends Component {
 		   let id = Ids[i];
 		   let coinInfo = await self.getInfo(id);
 		   let dataPoints = await self.getDataPoints(id);
+        this.setState({title: this.state.title + " ~ " + coinInfo.Name})
+		   
 		   
             data.push({
 				type: "line",
@@ -69,7 +88,7 @@ class LineChart extends Component {
 			theme: "light2", // "light1", "dark1", "dark2"
 			zoomEnabled: true,
 			title:{
-				text: "Historical Chart"
+				text: "Historical Chart: " + this.state.title
 			},
 			axisY: {
 				title: "USD",
@@ -98,6 +117,7 @@ class LineChart extends Component {
 			<CanvasJSChart options = {options} 
 				/* onRef={ref => this.chart = ref} */
 			/>
+			<button onClick={this.normalizeChart}>Normalize the chart</button>
 			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
 		</div>
 		);
