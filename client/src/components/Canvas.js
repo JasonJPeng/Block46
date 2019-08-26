30ac89bac0c7d065a0b06736b8b09038cefae663
Jason Peng <jason.j.peng@gamil.com>
2019-08-25 17:34:06 -0700

    new structure of canvas.js

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -10,46 +10,51 @@ class LineChart extends Component {
 	state = {
 		isNorm: false,
 		title: "",
-        chartInfo: {
-	      symbols: [],		
-		  data : [],
-		  prices: [],
-		  norm: [],
-		  originDataPoints: []
-		}  
+		data: [],
+		prices: [],
+		symbols: [],
+		norm: []
 	}
+
+	// 
+	originData = [];
 	
 	componentDidMount() {
         this.getData(this.props.Ids);
+	}
+
+	undoNormChart = (event) => {
+		event.preventDefault();
+		if (!this.state.isNorm)  return;
+
+		let newData = JSON.parse(JSON.stringify(this.originData))
+		let newTitle = this.state.symbols.join(" / ")
+
+		this.setState({data:newData, title:newTitle, isNorm:false})
 	}
 	
 	normalizeChart = (event) => {
 		event.preventDefault();
 		if (this.state.isNorm) return;
 
-		let newData = [];
-		console.log("-------->", this.state.chartInfo)
-		for (let i=0; i<this.state.chartInfo.data.length; i++) {
-			let element = this.state.chartInfo.data[i];
-			element.dataPoints = this.state.chartInfo.originDataPoints[i].map(ele=>{ 
-														   ele.y = ele.y / this.state.chartInfo.norm[i];
-														   return ele;
-														});
-			newData.push(element)
-		}
-		let newChartInfo = this.state.chartInfo;
-		newChartInfo.data = newData;
-		let newSym = this.state.chartInfo.symbols.map( (e,i) => {return 1/this.state.chartInfo.norm[i]+ " x " + e})
+		let newData = this.state.data.map( (ele, idx)=>{
+			    ele.dataPoints = ele.dataPoints.map(ele1=>{ 
+					ele1.y = ele1.y/this.state.norm[idx];
+					return ele1;
+				})
+			return ele;
+		})
+
+		let newSym = this.state.symbols.map( (e,i) => {return 1/this.state.norm[i]+ " x " + e})
 		let newTitle = newSym.join(" / ")
-		this.setState({chartInfo:newChartInfo, isNorm: true, title: newTitle} )		
+		
+		this.setState({data:newData, title:newTitle, isNorm:true})
 	}
 
 	getDataPoints = (id) => {
 		return new Promise((resolve, reject) => {
 		let self = this	;
-		axios.get("/api/coins/history/" + id).then (function(history){
-			console.log(history.data);
-			
+		axios.get("/api/coins/history/" + id).then (function(history){			
 			let dataPoints = []
             for (let i=1; i<history.data.Time.length; i++) {
 			   dataPoints.push({
@@ -73,40 +78,34 @@ class LineChart extends Component {
 	
 	getData = async (Ids) => {
 		// let id = Ids[0];
-		let chartInfo = {};
-		let data = [];
-		let prices = [];
-		let title = "";
-		let norm = [];
-		let symbols = [];
-		let originDataPoints = [];
+		let data = [], prices=[], title="", norm=[], symbols=[];
+	
 		for (let i=0; i< Ids.length; i++) {
 		   let self = this
 		   let id = Ids[i];
 		   let coinInfo = await self.getInfo(id);
 		   let dataPoints = await self.getDataPoints(id);
-		//    title = title + coinInfo.Name + " / ";
-		   console.log("Price----->", coinInfo)
-			prices.push(coinInfo.Price)
-			symbols.push(coinInfo.Symbol)
-            data.push({
+			 prices.push(coinInfo.Price)
+			 symbols.push(coinInfo.Symbol)
+             data.push({
 				type: "line",
 				showInLegend: true, 
                 legendText: ` (${coinInfo.Symbol}-${coinInfo.Name})=>$${coinInfo.Price} / `,
 				toolTipContent: `${coinInfo.Symbol} {x} $ {y}`,
 				dataPoints: dataPoints
 			})
-			originDataPoints.push(dataPoints);
-			// 	let maxValue = Math.max(...this.state.data[i].dataPoints.map(element=>{return element.y}));
-			// 1 to 10 normalization  
 			let maxValue = Math.max(...dataPoints.map(element=>element.y))
 			norm.push(10 ** parseInt(Math.log10(maxValue)));
 		}
+
+        // Deep Copy of the original data
+		this.originData = JSON.parse(JSON.stringify(data));
+
 		let minNorm = Math.min(...norm)
 		norm = norm.map(e=>e/minNorm) // use cheap coin as base
-		chartInfo = {data, symbols, norm, prices, originDataPoints}
+	
         title = symbols.join(" / ")
-		this.setState({title, chartInfo})
+		this.setState({title, data, prices, norm, symbols})
 	}
 
 
@@ -114,7 +113,7 @@ class LineChart extends Component {
 		const options = {
 			animationEnabled: true,
 			exportEnabled: true,
-			theme: "light2", // "light1", "dark1", "dark2"
+			theme: "light1", // "light1", "dark1", "dark2"
 			zoomEnabled: true,
 			title:{
 				text: this.state.title,
@@ -131,11 +130,11 @@ class LineChart extends Component {
 				prefix: " "
 				// interval: 2
 			},
-			data: this.state.chartInfo.data
+			data: this.state.data
 			// data: [{
 			// 	type: "line",
 			// 	toolTipContent: " {x}: $ {y}",
-			// 	dataPoints: this.state.data
+			// 	dataPoints: [ {x:01, y:22.3}, {}, {}, ...]
 			// }]
 		}
 
@@ -146,9 +145,12 @@ class LineChart extends Component {
 			<CanvasJSChart options={options} 
 				/* onRef={ref => this.chart = ref} */
 			/>
+			{this.state.isNorm?
+			<button onClick={this.undoNormChart}>Original Chart</button>:
 			<button onClick={this.normalizeChart}>Normalize the chart</button>
+			}
 			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		    <NewsInfo Ids={this.props.Ids} prices={this.state.chartInfo.prices} />
+		    {/* <NewsInfo Ids={this.props.Ids} prices={this.state.chartInfo.prices} /> */}
 		</div>
 		);
 	}
